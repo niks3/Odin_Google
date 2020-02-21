7fdb6e56ac78f8f9adf47b44c0bf2a50b56df62f
Nikola Radovic <nikolapokemon@gmail.com>
2020-02-21 22:38:34 +0100

    dodat python

---
+++ domaci.py
@@ -0,0 +1,53 @@
+""" a = int(input())
+b= int(input())
+
+if (a%2 == 0):
+    a +=1
+    print(a)
+if(b%2 == 0):
+    b -=1
+
+while(b >= a):
+    print(b-2)
+    b -=2  """
+
+""" a = int(input())
+b= int(input())
+c= 0
+i = a 
+while(i<b):
+    c = i+1
+    while(c <= b):
+        d = i*i + c*c
+        if(d%3 ==0 and d%5 != 0):
+            print(d)
+        c +=1
+    i +=1  
+ """
+""" a = int(input())
+i = 1
+while (i <= a//2):
+    if(a%i == 0): 
+        print(i)
+    i +=1
+print(a) """
+
+""" n = int(input())
+
+br = n*3 - 1
+print(br)
+ """
+
+""" 
+a = int(input())
+
+i = 10
+broj = 1 
+
+while( a // i != 0 ):
+    broj +=1
+    i *=10
+  
+print(broj)
+ """
+    
